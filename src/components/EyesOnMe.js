04f276ed72dea9750b1569f260f1cc6f793040b5
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-26 10:47:40 -0500

    Completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe({}) {
+  function onFocus(event) {
+    console.log("Good!")
+  }
+  function onBlur(event) {
+    console.log("Hey! Eyes on me!")
+  }
+
+  return (
+    <button onFocus={onFocus} onBlur={onBlur}>Eyes on me</button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
+import React from "react";
+
 // Code Keypad Component Here
 
 function Keypad (){
-    return (
-        <div></div>
-    )
+
+  function onChange(event) {
+    console.log("Entering password...")
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={onChange}></input>
+    </div>
+  )
 }
 
 export default Keypad;
